38c984513a446f14adcc30c16516f178806ab18a
chanchiuxyz <chanchiuxyz@gmail.com>
2024-07-31 19:13:22 -0700

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Next.js Dashboard',
-  description: "Next.js Dashboard, built with App Router",
+  description: "chiu chan, Next.js Dashboard, built with App Router",
   metadataBase: new URL("https://nextjs-dashboard.vercel.app"),
 }
 export default function RootLayout({
